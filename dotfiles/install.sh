ce41f6ab52c0729da4e9cace8a8bdde5d86d54eb
Pablo Caldentey <pablo@caldentey.org>
2017-08-16 11:42:13 +0200

    Added .gitignore_global file. Run 'git config --global core.excludesfile ~/.gitignore_global' to let git use this file

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -23,6 +23,17 @@ echo "Installing new .ctags file"
 folder=$(readlink -f ./.ctags)
 ln -s $folder ~/.ctags
 
+if [ -f ~/.gitignore_global  ]; then
+    mv ~/.gitignore_global ~/.gitignore_global.old
+    echo "Moving old ~/.gitignore_global file to ~/.gitignore_global"
+fi
+
+folder=$(readlink -f ./.ctags)
+ln -s $folder ~/.ctags
+
+echo "Installing new .gitignore_global file"
+echo -e "\e[1;31mRun git config --global core.excludesfile ~/.gitignore_global\e[0m";
+
 if [ -f ~/.gitconfig  ]; then
     mv ~/.gitconfig ~/.gitconfig.old
     echo "Moving old ~/.gitconfig file to ~/gitconfig.old"
